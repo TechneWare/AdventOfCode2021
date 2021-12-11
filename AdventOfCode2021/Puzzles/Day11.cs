e92155782447ee4819cd894f8dcf63865dabb76e
Brian Wham <brian.wham@techneware.com>
2021-12-11 10:19:03 -0500

    Add Day 11

---
+++ AdventOfCode2021/Puzzles/Day11.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2021.Puzzles
+{
+    /// <summary>
+    /// --- Day 11: Dumbo Octopus ---
+    /// <see cref="https://adventofcode.com/2021/day/11"/>
+    /// </summary>
+    public class Day11 : Puzzle
+    {
+        public Day11()
+            : base(Name: "Dumbo Octopus", DayNumber: 11) { }
+
+        public override void Part1(bool TestMode)
+        {
+            Part1Result = "Not Implemented";
+        }
+
+        public override void Part2(bool TestMode)
+        {
+            Part2Result = "Not Implemented";
+        }
+    }
+}
