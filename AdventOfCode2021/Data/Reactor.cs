4aa051a82a4bdff24fca8a23f93dac53ff91a13e
Brian Wham <brian.wham@techneware.com>
2021-12-22 18:47:46 -0500

    Add Day 22

---
+++ AdventOfCode2021/Data/Reactor.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2021.Data
+{
+    public class Reactor
+    {
+        public static List<RebootStep> RebootInstructions { get; set; } = new List<RebootStep>();
+
+        public static void LoadData(bool TestMode, int TestPart)
+        {
+            var fileName = $@"{Environment.CurrentDirectory}/Data/Files/ReactorCubesInput.txt";
+            if (TestMode)
+                if (TestPart == 1)
+                    fileName = $@"{Environment.CurrentDirectory}/Data/Files/ReactorCubesTestInput.txt";
+                else
+                    fileName = $@"{Environment.CurrentDirectory}/Data/Files/ReactorCubesTestInput_Part2.txt";
+
+            RebootInstructions.Clear();
+            var dataRaw = File.ReadAllText(fileName);
+            var lines = dataRaw.Split("\r\n");
+            foreach (var line in lines)
+            {
+
+                bool TurnOn = false;
+                var s = line.Split(' ');
+
+                if (s[0] == "on")
+                    TurnOn = true;
+
+                var c = s[1].Split(',');
+                var x = c[0].Replace("x=", "").Split("..");
+                var y = c[1].Replace("y=", "").Split("..");
+                var z = c[2].Replace("z=", "").Split("..");
+
+                var newStep = new RebootStep(!TurnOn,
+                                      new Cube(
+                                        new Segment(int.Parse(x[0]), int.Parse(x[1])),
+                                        new Segment(int.Parse(y[0]), int.Parse(y[1])),
+                                        new Segment(int.Parse(z[0]), int.Parse(z[1]))));
+
+                RebootInstructions.Add(newStep);
+            }
+        }
+    }
+    public record RebootStep(bool TurnOff, Cube Region);
+
+    public record Segment(int P1, int P2)
+    {
+        public bool IsEmpty => P1 > P2;
+        public long Length => IsEmpty ? 0 : P2 - P1 + 1;
+        public Segment Intersect(Segment other) => new(Math.Max(P1, other.P1), Math.Min(P2, other.P2));
+    }
+
+    public record Cube(Segment X, Segment Y, Segment Z)
+    {
+        public bool IsEmpty => X.IsEmpty || Y.IsEmpty || Z.IsEmpty;
+        public long Volume => X.Length * Y.Length * Z.Length;
+
+        public Cube Intersect(Cube other) => new(X.Intersect(other.X),
+                                                     Y.Intersect(other.Y),
+                                                     Z.Intersect(other.Z));
+    }
+}
--- AdventOfCode2021/Puzzles/Day22.cs
+++ AdventOfCode2021/Puzzles/Day22.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AdventOfCode2021.Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,22 +8,54 @@ using System.Threading.Tasks;
 namespace AdventOfCode2021.Puzzles
 {
     /// <summary>
-    /// 
-    /// <see cref=""/>
+    /// --- Day 22: Reactor Reboot ---
+    /// <see cref="https://adventofcode.com/2021/day/22"/>
     /// </summary>
     public class Day22 : Puzzle
     {
         public Day22()
-            : base(Name: "", DayNumber: 22) { }
+            : base(Name: "Reactor Reboot", DayNumber: 22) { }
 
         public override void Part1(bool TestMode)
         {
-            
+            Data.Reactor.LoadData(TestMode, TestPart: 1);
+            long total = Data.Reactor.RebootInstructions.RunReactorReboot(maxRange: 50);
+
+            Part1Result = $"Cubes= {total}";
         }
 
         public override void Part2(bool TestMode)
         {
-            
+            Data.Reactor.LoadData(TestMode, TestPart: 2);
+            var total = Data.Reactor.RebootInstructions.RunReactorReboot(maxRange: int.MaxValue);
+
+            Part2Result = $"Cubes= {total}";
+        }
+    }
+
+    public static class ReactorExtensions
+    {
+        public static long RunReactorReboot(this List<RebootStep> steps, int maxRange)
+        {
+
+            long GetCountAfterStep(int stepIndex, Cube region)
+            {
+                if (region.IsEmpty || stepIndex < 0)
+                    return 0;
+                else
+                {
+                    Cube RegionIntersect = region.Intersect(steps[stepIndex].Region);
+                    long OnInRegion = GetCountAfterStep(stepIndex - 1, region);
+                    long OnInIntersect = GetCountAfterStep(stepIndex - 1, RegionIntersect);
+                    long OnOutsideIntersect = OnInRegion - OnInIntersect;
+
+                    return steps[stepIndex].TurnOff ? OnOutsideIntersect : OnOutsideIntersect + RegionIntersect.Volume;
+                }
+            }
+
+            return GetCountAfterStep(steps.Count - 1, new Cube(new Segment(-maxRange, maxRange),
+                                                               new Segment(-maxRange, maxRange),
+                                                               new Segment(-maxRange, maxRange)));
         }
     }
 }
